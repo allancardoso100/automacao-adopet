69c4b3e6efb7164443a66122f4294991f3b54f51
Allan Cardoso <allancardoso100@gmail.com>
2024-08-12 20:57:06 -0300

    Subindo cenário CT03 - Visite a página de login do Adopet

--- cypress/e2e/pets_disponiveis.spec.cy.js
+++ cypress/e2e/pets_disponiveis.spec.cy.js
@@ -8,8 +8,14 @@ describe('CT01 - Visualize os pets disponíveis', () => {
 describe('CT02 - Teste de botões do cabeçalho', () => {
   it('Acesse a home e clique nos botões disponíveis no cabeçalhado da página', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
-   // cy.contains('a','Ir para Mensagens').click();
     cy.get('.header__message').click();
     cy.get('.header__home').click()
+  })
+})
+
+describe('CT03 - Visite a página de login do Adopet', () => {
+  it('Acesse a home e clique no botão Fazer Login', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.contains('a','Fazer login').click();
   })
 })
