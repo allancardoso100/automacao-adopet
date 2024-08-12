8d70a15fa664c89c54c086410cd9f541e4680ebb
Allan Cardoso <allancardoso100@gmail.com>
2024-08-12 17:07:33 -0300

    Subindo projeto

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,7 @@ describe('Página de cadastro', () => {
   it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
     cy.contains('a','Cadastrar').click();
-    cy.get('input[name="nome"]').type('Allan Cardoso');
+    cy.get('input[name="nome"]').type('Allan Cardoso Messias');
     cy.get('input[name="email"]').type('allan@gmail.com');
     cy.get('input[name="password"]').type('Senha123');
     cy.get('input[name="confirm_password"]').type('Senha123');
