8665ea5bcb2db92ce24a1ce64e7aab17d1805929
Allan Cardoso <allancardoso100@gmail.com>
2024-08-12 20:53:09 -0300

    Subindo cenário CT02 - Teste de botões do cabeçalho

--- cypress/e2e/pets_disponiveis.spec.cy.js
+++ cypress/e2e/pets_disponiveis.spec.cy.js
@@ -1,6 +1,15 @@
-describe('Visualize os pets disponíveis', () => {
+describe('CT01 - Visualize os pets disponíveis', () => {
   it('Acesse a home e clique no botão de pets disponiveis', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
     cy.contains('a','Ver pets disponíveis para adoção').click();
+  })
+})
+
+describe('CT02 - Teste de botões do cabeçalho', () => {
+  it('Acesse a home e clique nos botões disponíveis no cabeçalhado da página', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+   // cy.contains('a','Ir para Mensagens').click();
+    cy.get('.header__message').click();
+    cy.get('.header__home').click()
   })
 })
