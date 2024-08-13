018320981fc24eab2cbc2c1279cc8fc9d2474e7d
Allan Cardoso <allancardoso100@gmail.com>
2024-08-12 22:36:15 -0300

    Melhorando captura de elementos com propriedades do Cypress

---
+++ cypress/e2e/cadastro-correto.cy.js
@@ -0,0 +1,11 @@
+describe('Página de cadastro', () => {
+  it('CT00 - Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/');
+    cy.get('[data-test="register-button"]').click();
+    cy.get('[data-test="input-name"]').type('Allan Cardoso');
+    cy.get('[data-test="input-email"]').type('allan@gmail.com');
+    cy.get('[data-test="input-password"]').type('Senha123');
+    cy.get('[data-test="input-confirm-password"]').type('Senha123');
+    cy.get('[data-test="submit-button"]').click();
+  })
+})
