9cbec9459bc14724665a50573017b94dee89908d
Allan Cardoso <allancardoso100@gmail.com>
2024-08-12 21:20:09 -0300

    Subindo ajustes nos elementos de captura dos cenários

--- cypress/e2e/pets_disponiveis.spec.cy.js
+++ cypress/e2e/pets_disponiveis.spec.cy.js
@@ -1,7 +1,7 @@
 describe('CT01 - Visualize os pets disponíveis', () => {
   it('Acesse a home e clique no botão de pets disponiveis', () => {
     cy.visit('https://adopet-frontend-cypress.vercel.app/');
-    cy.contains('a','Ver pets disponíveis para adoção').click();
+    cy.get('.button').click()
   })
 })
 
