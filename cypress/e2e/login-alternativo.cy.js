9b5f2ad8b5a8bc99a762bc262846de95cb923a66
Allan Cardoso <allancardoso100@gmail.com>
2024-08-15 23:27:58 -0300

    Subindo cenários complementares e beforeEach

--- cypress/e2e/login-alternativo.cy.js
+++ cypress/e2e/login-alternativo.cy.js
@@ -4,6 +4,7 @@ describe('Pagina de login', () => {
         cy.get('.header__message').click();
         //No caso acima, foi utilizado o cy.get, pois o botão não tem um texto VISÍVEL na página para utilizar o contains
     })
+    
     it('CT08 - Deve acessar a página de login atraves do ícone de mensagem da home', () => {
         cy.get('[data-test="input-loginEmail"]').type('allan@gmail.com');
         cy.get('[data-test="input-loginPassword"]').type('Senha123');
