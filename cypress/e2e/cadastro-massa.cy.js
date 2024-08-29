68da5690da477d2621deea2b42c1ee14f1c0707b
Allan Cardoso <allancardoso100@gmail.com>
2024-08-29 14:26:59 -0300

    Subindo cenários com consumo de massa de teste em aquivo json

---
+++ cypress/e2e/cadastro-massa.cy.js
@@ -0,0 +1,21 @@
+import { usuarios } from '../fixtures/usuarios.json'
+
+describe('Página de cadastro com multiplos usuários', () => {
+    beforeEach(() => {
+        cy.visit('https://adopet-frontend-cypress.vercel.app')
+        cy.get('[data-test="register-button"]').click();
+    })
+
+    usuarios.forEach(usuario => {
+        it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+            cy.get('[data-test="input-name"]').clear().type(usuario.name);
+            cy.get('[data-test="input-email"]').clear().type(usuario.email);
+            cy.get('[data-test="input-password"]').clear().type(usuario.password);
+            cy.get('[data-test="input-confirm-password"]').clear().type(usuario.password);
+            cy.get('[data-test="submit-button"]').click();
+        })
+    })
+})
+
+
+    
